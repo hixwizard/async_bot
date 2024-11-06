04d84709237ff87b3baa811248d64ea53205ba53
hix9 <stanislavbarinov0@gmail.com>
2024-11-06 19:42:29 +0300

    Оптимизировать функции в коде

--- src/bot_app/buttons.py
+++ src/bot_app/buttons.py
@@ -7,12 +7,14 @@ from telegram import (
 
 def start_keyboard() -> ReplyKeyboardMarkup:
     """Возвращает клавиатуру с кнопками 'Создать заявку' и 'Мои заявки'."""
-    keyboard = [['Создать заявку', 'Мои заявки']]
-    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+    return ReplyKeyboardMarkup(
+        [['Создать заявку', 'Мои заявки']],
+        one_time_keyboard=True
+    )
 
 
 def contact_keyboard() -> InlineKeyboardMarkup:
     """Создает кнопку для начала нового опроса."""
-    keyboard = [[InlineKeyboardButton("Да",
-                                      callback_data="start_survey")]]
-    return InlineKeyboardMarkup(keyboard)
+    return InlineKeyboardMarkup(
+        [[InlineKeyboardButton("Да", callback_data="start_survey")]]
+    )
