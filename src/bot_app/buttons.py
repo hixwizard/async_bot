28d74e7778ac7697975bcbba50505ad93c8a99fe
hix9 <stanislavbarinov0@gmail.com>
2024-11-06 19:45:40 +0300

    Оптимизировать функции в коде

--- src/bot_app/buttons.py
+++ src/bot_app/buttons.py
@@ -9,12 +9,12 @@ def start_keyboard() -> ReplyKeyboardMarkup:
     """Возвращает клавиатуру с кнопками 'Создать заявку' и 'Мои заявки'."""
     return ReplyKeyboardMarkup(
         [['Создать заявку', 'Мои заявки']],
-        one_time_keyboard=True
+        one_time_keyboard=True,
     )
 
 
 def contact_keyboard() -> InlineKeyboardMarkup:
     """Создает кнопку для начала нового опроса."""
     return InlineKeyboardMarkup(
-        [[InlineKeyboardButton("Да", callback_data="start_survey")]]
+        [[InlineKeyboardButton("Да", callback_data="start_survey")]],
     )
