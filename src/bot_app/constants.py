9c463b7791577b800a416eb20e2207f7fd8e5ca4
hix9 <stanislavbarinov0@gmail.com>
2024-11-22 19:13:08 +0300

    Доработать константы в bot.py

--- src/bot_app/constants.py
+++ src/bot_app/constants.py
@@ -1,11 +1,15 @@
-WELCOME: str = 'Добро пожаловать! Выберите нужное действие:'
+# Логические константы
 FIRST_QUESTION: int = 0
+NEXT_QUESTION: int = 1
+NEXT_NUMBER: int = 1
+SELECTED_FIELD: int = 1
+
+# Информационные сообщения
+WELCOME: str = 'Добро пожаловать! Выберите нужное действие:'
 HAVENT_QUESTIONS: str = 'Вопросы для опроса отсутствуют.'
 MESSAGE_NOT_FOUND: str = 'Произошла ошибка: сообщение не найдено.'
-NEXT_QUESTION: int = 1
 DEFAULT_STATUS: str = 'открыта'
 SUCCESSFUL_SAVE: str = 'Заявка успешно сохранена!'
-NEXT_NUMBER: int = 1
 ASK_FOR_CONTACTS: str = 'Пожалуйста, укажите контактные данные.\nВведите ваш номер телефона или email:'# noqa
 CHOOSE_EDIT_QUESTION: str = 'Пожалуйста, выберите вопрос для редактирования:'
 CHOOSE_EDIT_OR_OK: str = 'Пожалуйста, нажмите «Подтвердить» или «Редактировать» для продолжения.'# noqa
@@ -17,7 +21,6 @@ SUCCESSFUL_EDIT: str = 'Ваши ответы подтверждены.'
 NOTHING_TO_EDIT: str = 'Нет вопросов для редактирования.'
 QUESTION_NOT_FOUND: str = 'Вопрос не найден.'
 CHOOSE_TO_EDIT: str = 'Выберите, что вы хотите редактировать: '
-SELECTED_FIELD: int = 1
 PROFILE_UPDATED: str = 'Ваш профиль успешно обновлен.'
 BLOCK_MESSAGE: str = 'Вы заблокированы. Обратитесь к администратору. '
 USER_NOT_FOUND: str = 'Пользователь не найден.'
